3756966682e20f6badb8798ee03782b21572aff2
King-rover123 <167163131+King-rover123@users.noreply.github.com>
2024-04-16 06:05:06 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['2347041055226']
+global.ownername = "90MS-CODM"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Nigeria"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = '90MS-𝐌𝐃' //name of the bot
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
